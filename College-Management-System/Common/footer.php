2d06c3afa678374490b8d514490c6fb4f9f6dabe
saadmalik8806 <saadmalik8806@gmail.com>
2022-02-22 17:55:05 +0530

    HI
    PROJECT

---
+++ College-Management-System/Common/footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Imperial College</h3>
+            <p>This Project has been made by Saad Malik.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About ICBS</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Sandip University, Mahiravani, Tal & Dist: Nashik - 422213, Trambakeshwar Rd, Nashik, Maharashtra 422213, India</p>
+              <p>Telephones: +91 1800 212 2714</p>
+              <p>E-mail: info@sandipuniversity.edu.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Saad Malik.2022</p>
+</footer>
---
+++ College-Management-System/Connection/connection.php
@@ -0,0 +1,7 @@
+<?php 
+	$con=mysqli_connect("localhost","root","","sandip_university");
+	if(!$con)
+	{
+		echo "Connection is not Successfully";
+	}
+?>
